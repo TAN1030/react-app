b05b00eebfa03d397adc6f1c1b095e55f36add57
TAN1030 <tangerine.k@outlook.kr>
2023-05-27 19:31:28 +0900

    [router-dom TEST] 쿼리스트링 - useLocation 테스트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './page/Header';
-import Main from './page/Main';
-import Product from './page/Product';
-import NotFound from './page/NotFound';
+import Header from './views/Header';
+import Main from './views/Main';
+import Product from './views/Product';
+import NotFound from './views/NotFound';
 
 const App = () => {
 	return (
@@ -11,8 +11,8 @@ const App = () => {
       <BrowserRouter>      
         <Header />
         <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="/product/*" element={<Product />}></Route>
+        <Route path="/" element={<Main />}></Route>
+          <Route path="/product/:productId" element={<Product />}></Route>
           { /* 상단에 위치하는 Route 들을 확인하고 일치하는 Route 가 없다면, 아래와같이 처리 */}
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Main = (props) => {
+const Main = () => {
 	return (
 		<>
-			<h3>메인페이지 입니다.</h3>
-
+			<h3>메인페이지</h3> 
             <ul>
-                <Link to="/product/1"><li>1번상품</li></Link>
-                <Link to="/product/2"><li>2번상품</li></Link>                
+                <Link to="/product/1"><li>1번 상품</li></Link>
+                <Link to="/product/2"><li>2번 상품</li></Link>          
+				<Link to="/product/3?search=productName&q=demo#test"><li>3번 상품</li></Link>          
+				      
             </ul>
 		</>
 	);
