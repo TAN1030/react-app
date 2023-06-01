771affd80cafec99168e0c1bf286cdc0b8d769e4
TAN1030 <tangerine.k@outlook.kr>
2023-06-01 17:49:42 +0900

    [React-js-pagination TEST] react-paginate 라이브러리 적용 (세부내용 수정필요)

--- src/views/ListCase/Posts.js
+++ src/views/ListCase/Posts.js
@@ -1,28 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useSearchParams } from 'react-router-dom'
-import PostList from '../components/PostList';
- 
+import PostList from '../components/PostList'
+import Paging from '../components/Paging'
+
+
 const Posts = () => { 
 
     const [searchParams, setSearchParams] = useSearchParams();
     const offset = searchParams.get('offset') === null? 0 : searchParams.get('offset');
-    const limit  = searchParams.get('limit') === null? 10 : searchParams.get('limit');
+    const limit  = searchParams.get('limit') === null? 7 : searchParams.get('limit');
 
     // BACK
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_start=${offset}`)
+//        axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_start=${offset}`)
+          axios.get(`https://jsonplaceholder.typicode.com/posts`)
             .then(response => { 
                 console.log(response); 
                 setPosts(response.data); });  
     }, []);
+
+
     // 자식 컴포넌트인 UserList에 users데이터를 props형태로 전달
     return (
         < >
-        <h1>Users</h1>
-        <PostList posts={posts}/>
+        <h5>~ post list ~</h5>
+        <PostList itemsPerPage={10} posts={posts}/>
         </>
     );
 };
--- src/views/components/PostList.js
+++ src/views/components/PostList.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactPaginate from 'react-paginate';  // https://www.npmjs.com/package/react-paginate
+import "./Paging.css"
 
-const PostList = ({ posts }) => {
+const PostList = ({itemsPerPage, posts }) => {
+
+    itemsPerPage = itemsPerPage === undefined ? 7 : itemsPerPage;
+    const items = posts;
+
+    const [itemOffset, setItemOffset] = useState(0);
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    const currentItems = items.slice(itemOffset, endOffset); // 현재 페이징 그룹
+    const pageCount = Math.ceil(items.length / itemsPerPage);
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % items.length;
+        console.log(
+          `User requested page number ${event.selected}, which is offset ${newOffset}`
+        );
+        setItemOffset(newOffset);
+      };
     return (
         <>
         <div>
-        {posts.map(post => {
-            return (<div key={post.id}> 1 </div>)
+        {currentItems && currentItems.map(post => {
+            return (<div key={post.id}>[{post.id}] {post.title} </div>)
         })}
         </div>
-        </>
+    
+    <div>
+      <ReactPaginate
+      breakLabel="..."
+      nextLabel="next >"
+      onPageChange={handlePageClick}
+      pageRangeDisplayed={5}
+      pageCount={pageCount}
+      previousLabel="< previous"
+      renderOnZeroPageCount={null}/>
+    
+    </div>
+    <br/>
+<hr/>
+<br/>
+    </>
     );
 };
 
