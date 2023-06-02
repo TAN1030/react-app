078faf9180e1ac28afb92247149db895781c655c
TAN1030 <tangerine.k@outlook.kr>
2023-06-02 15:20:35 +0900

    [React-js-pagination TEST] react-paginate 라이브러리 적용 (css 수정)

--- src/views/components/PostList.js
+++ src/views/components/PostList.js
@@ -28,16 +28,28 @@ const PostList = ({itemsPerPage, posts }) => {
         })}
         </div>
     
-    <div>
+    <div class="pager" style={{
+                display: 'flex'
+      }}>
       <ReactPaginate
-      breakLabel="..."
-      nextLabel="next >"
-      onPageChange={handlePageClick}
-      pageRangeDisplayed={5}
-      pageCount={pageCount}
-      previousLabel="< previous"
-      renderOnZeroPageCount={null}/>
-    
+      pageCount={pageCount}  // 필수. 총페이지수 
+      previousLabel=" ◀ " // 버튼레이블 - previous 
+      breakLabel="..."  // 줄임표 레이블
+      nextLabel=" ▶ "   // 버튼레이블 - next 
+
+      pageRangeDisplayed={3}     // 표시되는 페이지 범위
+      marginPagesDisplayed = {3} // 여백에 표시되는 범위
+      breakClassName="This_is__li:breakLabel__name" // li줄임표 요소의 태그에 있는 클래스 이름
+      breakLinkClassName="This_is__a:breakLabel__name" //a줄임표 요소의 태그에 있는 클래스 이름
+      className="This_is__className"
+      pageClassName="This_is__li___name"
+      pageLinkClassName="This_is__a:li__name"
+
+      activeClassName="THIS_IS_NOW_PAGE"  // 활성 페이지의 클래스 이름
+      activeLinkClassName="THIS_IS_NOW_PAGE_A" // 활성 페이지의 a태그 이름
+      renderOnZeroPageCount={null} // 0일 때 호출되는 렌더링 함수
+      onPageChange={handlePageClick}  // 페이지가 변경될때 호출하는 메서드.  현재 페이지 개체를 인수로 노출
+/>  
     </div>
     <br/>
 <hr/>
